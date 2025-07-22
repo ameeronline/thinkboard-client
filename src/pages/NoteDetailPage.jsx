51af65b61bb06b3ac7e7477828064b80e9e194c8
ameeronline <helloameeronline@gmail.com>
2025-07-22 21:26:12 +0530

    Note details and edit

--- src/pages/NoteDetailPage.jsx
+++ src/pages/NoteDetailPage.jsx
@@ -1,9 +1,65 @@
-import React from 'react'
+import { ArrowLeftIcon, Loader2Icon, Trash2Icon } from "lucide-react";
+import React, { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+import { Link, useNavigate, useParams } from "react-router";
+import axios from "axios";
+import Navbar from "../components/Navbar";
 
 const NoteDetailPage = () => {
-  return (
-    <div>NoteDetailPage</div>
-  )
-}
+  const [note, setNote] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [saving, setSaving] = useState(false);
 
-export default NoteDetailPage
+  const naviagate = useNavigate();
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchNote = async () => {
+      try {
+        const res = await axios.get(`${import.meta.env.VITE_API}/notes/${id}`);
+        setNote(res.data);
+      } catch (error) {
+        console.error("Error fetching notes", error);
+        toast.error("Failed to fetch notes");
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchNote();
+  }, [id]);
+
+  const handleDelete = async () => {};
+
+  if (loading) {
+    return (
+      <div className="min-screen bg-base-200 flex items-center justify-center">
+        <Loader2Icon className="animate-spin size-10" />
+      </div>
+    );
+  }
+
+  return (
+    <div className="min-h-screen bg-base-200">
+      <Navbar />
+      <div className="container mx-auto px-4 py-8">
+        <div className="max-w-2xl mx-auto">
+          <div className="flex items-center justify-between mb-6">
+            <Link to={"/"} className="btn btn-ghost">
+              <ArrowLeftIcon className="size-5" />
+              Back to Notes
+            </Link>
+            <button
+              onClick={handleDelete}
+              className="btn btn-error btn-outline"
+            >
+              <Trash2Icon className="h-5 w-5" />
+              Delete Note
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NoteDetailPage;
